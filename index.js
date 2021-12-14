9bcf2f0527af7245516e1e4f779b8a199856eac9
virtualxcce <cemal.efe@virtualitechnologies.com>
2021-12-14 16:40:46 +0300

    exported connections

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const db = require('./database');
+const con = require('./database/connections');
 const sm = require('./secretmanager');
 const km = require('./keymap');
 
 module.exports = {
     db,
+    con,
     sm,
     km,
     database: db,
+    connections: con,
     secretmanager: sm,
     keymap: km
 };
