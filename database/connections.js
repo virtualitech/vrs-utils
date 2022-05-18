6a4f8b727a34a7d580dfd24a791df7e52fcf9fc2
Cemal Can Efe <cemal.efe@virtualitechnologies.com>
2022-05-18 13:02:36 +0300

    use DB_CONNECTION_SECRET_PATH env variable

--- database/connections.js
+++ database/connections.js
@@ -1,9 +1,10 @@
 const secretManager = require('../secretmanager');
+const secretPath = process.env.DB_ENV_SECRET_PATH || 'projects/1088752406703/secrets/connections/versions/latest';
 let connectionInfo = null;
 
 module.exports = async function () {
     if (!connectionInfo) {
-        const connections = await secretManager('projects/1088752406703/secrets/connections/versions/latest');
+        const connections = await secretManager(secretPath);
         connectionInfo = JSON.parse(connections);
     }
 
