131a52495737e5459ef108190575f6635401a236
Cemal Can Efe <cemal.efe@virtualitechnologies.com>
2023-01-31 15:25:37 +0300

    added libraries

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const db = require('./database');
 const con = require('./database/connections');
 const sm = require('./secretmanager');
 const km = require('./keymap');
+const lib = require('./lib');
 
 module.exports = {
     db,
     con,
     sm,
     km,
+    lib,
     database: db,
     connections: con,
     secretmanager: sm,
