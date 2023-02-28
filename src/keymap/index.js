d566952c1ede47e6ceab77f0fe9613dbf360ee96
Cemal Can Efe <cemal.efe@virtualitechnologies.com>
2023-02-28 17:05:25 +0300

    corrected secretmanager path in keymap

--- src/keymap/index.js
+++ src/keymap/index.js
@@ -1,9 +1,10 @@
-const secretManager = require('../../secretmanager');
+const secretManager = require('../secretmanager');
+const secretPath = process.env.KEYMAP_SECRET_MANAGER_RESOURCE_ID || 'projects/1088752406703/secrets/keymap/versions/latest';
 let keys = null;
 
 module.exports = async function () {
     if (!keys) {
-        const map = await secretManager('projects/1088752406703/secrets/keymap/versions/latest');
+        const map = await secretManager(secretPath);
         keys = JSON.parse(map);
     }
 
