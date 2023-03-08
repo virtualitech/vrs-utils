b63e13997646244f6908c120c59b8fcaa600d434
Cemal Can Efe <cemal.efe@virtualitechnologies.com>
2023-03-08 14:11:13 +0300

    handle html response in request lib

--- src/lib/request.js
+++ src/lib/request.js
@@ -1,34 +1,56 @@
 const http = require('http');
 const https = require('https');
 const FormData = require('form-data');
+const { parse: htmlParser } = require('node-html-parser');
+const { parse: jsonParser } = JSON;
 
 module.exports = function (config) {
-	const { url, headers = {}, text, json, formData, method = 'POST', timeout = 60000, responseType = 'json' } = config;
+	const {
+		url,
+		headers = {},
+		text,
+		json,
+		formData,
+		method = 'post',
+		timeout = 60000,
+		responseType = 'json',
+		returnHeaders = false
+	} = config;
+
 	const protocol = url.startsWith('https') ? https : http;
 	const isMultipart = !!formData;
+	const isPost = method.toLowerCase() === 'post' || method.toLowerCase() === 'put';
 
 	let data;
 
-	if (isMultipart) {
-		data = new FormData();
+	if (isPost) {
+		if (isMultipart) {
+			data = new FormData();
 
-		for (var p in formData) {
-			const item = formData[p];
+			for (var p in formData) {
+				const item = formData[p];
 
-			if (typeof item === 'object' && Buffer.isBuffer(item.data)) {
-				data.append(p, item.data, { filename: encodeURI(item.name) || 'unknown' });
+				if (typeof item === 'object' && Buffer.isBuffer(item.data)) {
+					data.append(p, item.data, { filename: encodeURI(item.name) || 'unknown' });
+				}
+				else {
+					data.append(p, item);
+				}
 			}
-			else {
-				data.append(p, item);
-			}
+
+			Object.assign(headers, data.getHeaders());
+		}
+		else if (json) {
+			data = JSON.stringify(json, null, 2);
+			headers['Content-Type'] = 'application/json';
+		} else {
+			data = text;
+			headers['Content-Type'] = 'text/plain';
 		}
 
-		Object.assign(headers, data.getHeaders());
-	}
-	else {
-		data = json ? JSON.stringify(json, null, 2) : text;
-		headers['Content-Type'] = json ? 'application/json' : 'text/plain';
-		headers['Content-Length'] = data.length;
+		if (data) {
+			headers['Content-Length'] = data.length;
+		}
 	}
 
 	const options = {
@@ -40,7 +62,7 @@ module.exports = function (config) {
 
 	return new Promise((resolve, reject) => {
 		const req = protocol.request(url, options, (res) => {
-			const { statusCode } = res;
+			const { statusCode, headers } = res;
 
 			if (statusCode < 200 || statusCode > 299) {
 				return reject(new Error(`HTTP status code ${statusCode} ${url}`));
@@ -52,13 +74,22 @@ module.exports = function (config) {
 			res.on('end', () => {
 				try {
 					const resString = Buffer.concat(body).toString();
+					let data;
 
 					if (responseType === 'json') {
-						const resJson = JSON.parse(resString);
-						resolve(resJson);
+						data = jsonParser(resString);
+					}
+					else if (responseType === 'html') {
+						data = htmlParser(resString);
 					}
 					else {
-						resolve(resString);
+						data = resString;
+					}
+
+					if (returnHeaders) {
+						resolve({ data, headers });
+					} else {
+						resolve(data);
 					}
 				}
 				catch (err) {
@@ -76,11 +107,15 @@ module.exports = function (config) {
 			reject(new Error(`request TIMEDOUT ${url}`));
 		});
 
-		if (isMultipart) {
-			data.pipe(req);
-		}
-		else {
-			req.write(data);
+		if (isPost) {
+			if (isMultipart) {
+				data.pipe(req);
+			}
+			else {
+				req.write(data);
+				req.end();
+			}
+		} else {
 			req.end();
 		}
 	});
